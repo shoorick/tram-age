11fb00986d1577360a08c1233798a49cda559cd8
Alexander Sapozhnikov <shoorick@cpan.org>
2020-11-06 03:33:52 +0500

    Make GET request with fake User-Agent header

--- compute.py
+++ compute.py
@@ -1,14 +1,35 @@
 import sys
 import re
+import urllib.request
 
+"""
+Check for version of Python and exit with error when version is older than 3
+"""
 def check_version():
     major_version = int(re.search('^\d+', sys.version).group(0))
     if major_version < 3:
         sys.exit('Use version 3 at least\n')
 
+"""
+Get content from specified URL
+"""
+def get_content(url):
+    req = urllib.request.Request(
+        url,
+        data=None,
+        headers={
+            'User-Agent':
+                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' +
+                '(KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
+        }
+    )
+
+    f = urllib.request.urlopen(req)
+    return f.read().decode('utf-8')
+
 def main():
     check_version()
-    print('Not yet implemented')
+    print(get_content('https://transphoto.org/list.php?serv=0&cid=54&mid=1'))
 
 if __name__ == '__main__':
     main()
